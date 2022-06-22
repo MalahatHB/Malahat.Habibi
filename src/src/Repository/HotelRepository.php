84afc70093d05ae0c81ec9005ddeacc1bb93c9f8
MalahatHabibi <malahat.habibi2001@gmail.com>
2022-06-22 17:44:40 +0430

    Assignment 4 changes

---
+++ src/src/Repository/HotelRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Hotel;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Hotel>
+ * @method Hotel|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Hotel|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Hotel[]    findAll()
+ * @method Hotel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HotelRepository extends ServiceEntityRepository {
+
+    public function __construct(ManagerRegistry $registry) {
+        parent::__construct($registry, Hotel::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Hotel $entity, bool $flush = true): void {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Hotel $entity, bool $flush = true): void {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public function searchByName($hotelNamePart) {
+
+        $qb = $this->createQueryBuilder('h');
+
+        return $qb
+            ->andWhere(
+                $qb->expr()->like('h.name', ':name')
+            )
+            ->setParameter('name', "%$hotelNamePart%")
+            ->orderBy('h.name', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+}
